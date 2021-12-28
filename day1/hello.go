0df4a0a838b09e0f5a364f7c9ac4e8eb1aac12f2
0hlov3 <0hlov3@protonmail.com>
2021-12-29 00:11:13 +0100

    Learning Tests

--- day1/hello.go
+++ day1/hello.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello() string {
 	return "Hello, World"
 }
@@ -10,8 +12,21 @@ func HelloName(name string) string {
 	return "Hello, " + name
 }
 
+func HelloNameConst(name string) string {
+	return englishHelloPrefix + name
+}
+
+func HelloNameFallBack(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
+}
+
 func main() {
 	fmt.Println("Hello, world")
 	fmt.Println(Hello())
 	fmt.Println(HelloName("0hlov3"))
+	fmt.Println(HelloNameConst("0hlov3"))
+	fmt.Println(HelloNameFallBack("0hlov3"))
 }
--- day1/hello_test.go
+++ day1/hello_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestHello(t *testing.T) {
 	got := Hello()
@@ -18,4 +20,32 @@ func TestHelloName(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func TestHelloNameConst(t *testing.T) {
+	got := HelloNameConst("0hlov3")
+	want := "Hello, 0hlov3"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestHelloNameFallBack(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q wanr %q", got, want)
+		}
+	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := HelloNameFallBack("0hlov3")
+		want := "Hello, 0hlov3"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := HelloNameFallBack("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
